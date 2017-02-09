c381294f0c0429e1468b022e9cb6903467d6a0d5
Daniel Skowroński <daniel@dsinf.net>
2017-02-09 20:25:39 +0100

    quick and dirty transition to first model that needs to be supported - single Note

--- controllers/note.go
+++ controllers/note.go
@@ -6,59 +6,54 @@ import (
 
 	"github.com/gernest/utron/controller"
 	"github.com/gorilla/schema"
-	"github.com/utronframework/todo/models"
+	"../models"
 )
 
 var decoder = schema.NewDecoder()
 
-//Todo is a controller for Todo list
-type Todo struct {
+type Note struct {
 	controller.BaseController
 	Routes []string
 }
 
-//Home renders a todo list
-func (t *Todo) Home() {
-	todos := []*models.Todo{}
-	t.Ctx.DB.Order("created_at desc").Find(&todos)
-	t.Ctx.Data["List"] = todos
+func (t *Note) Home() {
+	notes := []*models.Note{}
+	t.Ctx.DB.Order("created_at desc").Find(&notes)
+	t.Ctx.Data["List"] = notes
 	t.Ctx.Template = "index"
 	t.HTML(http.StatusOK)
 }
 
-//Create creates a todo  item
-func (t *Todo) Create() {
-	todo := &models.Todo{}
+func (t *Note) Create() {
+	Note := &models.Note{}
 	req := t.Ctx.Request()
 	_ = req.ParseForm()
-	if err := decoder.Decode(todo, req.PostForm); err != nil {
+	if err := decoder.Decode(Note, req.PostForm); err != nil {
 		t.Ctx.Data["Message"] = err.Error()
 		t.Ctx.Template = "error"
 		t.HTML(http.StatusInternalServerError)
 		return
 	}
 
-	t.Ctx.DB.Create(todo)
+	t.Ctx.DB.Create(Note)
 	t.Ctx.Redirect("/", http.StatusFound)
 }
 
-//Delete deletes a todo item
-func (t *Todo) Delete() {
-	todoID := t.Ctx.Params["id"]
-	ID, err := strconv.Atoi(todoID)
+func (t *Note) Delete() {
+	NoteID := t.Ctx.Params["id"]
+	ID, err := strconv.Atoi(NoteID)
 	if err != nil {
 		t.Ctx.Data["Message"] = err.Error()
 		t.Ctx.Template = "error"
 		t.HTML(http.StatusInternalServerError)
 		return
 	}
-	t.Ctx.DB.Delete(&models.Todo{ID: ID})
+	t.Ctx.DB.Delete(&models.Note{ID: ID})
 	t.Ctx.Redirect("/", http.StatusFound)
 }
 
-//NewTodo returns a new  todo list controller
-func NewTodo() controller.Controller {
-	return &Todo{
+func NewNote() controller.Controller {
+	return &Note{
 		Routes: []string{
 			"get;/;Home",
 			"post;/create;Create",
